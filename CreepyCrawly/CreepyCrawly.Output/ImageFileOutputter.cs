6781cfba867ea7fe1723051084ecac392d4c3ce5
JurajDoncevic <Juraj@BELINA>
2019-11-27 10:28:15 +0100

    img outputter

---
+++ CreepyCrawly/CreepyCrawly.Output/ImageFileOutputter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CreepyCrawly.Output
+{
+    class ImageFileOutputter : IFileOutputter
+    {
+        public string BaseDirPath { get; private set; }
+        private int _FileCount;
+        public ImageFileOutputter(string baseDirPath)
+        {
+            BaseDirPath = baseDirPath;
+            _FileCount = 0;
+        }
+
+        public void WriteOutput(object output)
+        {
+            _FileCount++;
+            File.WriteAllBytes(BaseDirPath + "/" + _FileCount + ".png", (byte[])output);
+        }
+
+        public void WriteOutputWithName(object output, string fileName)
+        {
+
+        }
+    }
+}
